d018327052a2dd659914ec9dc078db32dd7c2313
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2018-12-05 23:06:21 +0530

    addeed header radio boxes

--- index.php
+++ index.php
@@ -5,4 +5,36 @@
  * Date: 5/12/18
  * Time: 10:15 PM
  */
+
 ?>
+<html>
+    <head><title>cv</title></head>
+    <body>
+        <div id="top_header">
+            <!--This is a top header which defines which html registration form should be selected.-->
+            <input type="radio" name="which_page" id="demographic">
+            <label for="demographic">Demographic</label>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+            <input type="radio" name="which_page" id="education">
+            <label for="education">Education</label>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+            <input type="radio" name="which_page" id="projects">
+            <label for="projects">Projects</label>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+            <input type="radio" name="which_page" id="certification">
+            <label for="certification">Certification</label>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+            <input type="radio" name="which_page" id="skill">
+            <label for="skill">Skills</label>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+            <input type="radio" name="which_page" id="employment">
+            <label for="employment">employment</label>
+        </div>
+
+    </body>
+</html>
