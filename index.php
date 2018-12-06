e0387e89c395d44cb92cef85fba8802ed00ac7e4
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2018-12-06 20:46:13 +0530

    next button not working

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-<html>
 <head>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="index.css">
-    <script src="index.js"></script>
+    <link rel="stylesheet" href="index.css">
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="index.js"></script
 </head>
-<body>
-
 <!-- multistep form -->
 <form id="msform">
     <!-- progressbar -->
@@ -17,17 +14,16 @@
         <li>Employment</li>
 
     </ul>
+
     <!-- fieldsets -->
     <fieldset>
-        <!--
-        <h2 class="fs-title">Pesonal Information</h2>
-        -->
+        <h2 class="fs-title">Create your account</h2>
         <h3 class="fs-subtitle">This is step 1</h3>
         <input type="text" name="Name" placeholder="Name" />
         <input type="address" name="address" placeholder="Address" />
         <input type="text" name="Email" placeholder="Email" />
         <input type="text" name="Website" placeholder="Website" />
-        <input type="button" name="next" class="next action-button" value="Next" />
+        <input type="button" name="next" class="next action-button" value="Next" id="name"/>
     </fieldset>
     <fieldset>
         <input type="text" name="HSC" placeholder="Education" />
@@ -39,9 +35,8 @@
         <input type="button" name="previous" class="previous action-button" value="Previous" />
         <input type="button" name="next" class="next action-button" value="Next" />
     </fieldset>
+
     <fieldset>
 
     </fieldset>
 </form>
-</body>
-</html>
