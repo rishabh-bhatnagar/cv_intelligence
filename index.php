c475c5c5b71bdcdcb916f8e654bcc1ef5816b536
Sonia Singla <ssoniyaster@gmail.com>
2018-12-09 19:44:13 +0000

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
 <!-- multistep form -->
 
-<form id="msform">
+<form id="msform"  onsubmit="return validateForm(this)">
 
     <!-- progressbar -->
 
@@ -45,15 +45,16 @@
 
         <h3 class="fs-subtitle">This is step 1</h3>
 
-        <input type="text" name="Name" placeholder="Name" />
+        <input type="text" name="Name" required aria-required="true" pattern="[A-Za-z-0-9]+\s[A-Za-z-'0-9]+" placeholder="Name" />
 
-        <input type="address" name="address" placeholder="Address" />
+        <input type="address" name="address"required  placeholder="Address" />
 
-        <input type="text" name="Email" placeholder="Email" />
+        <input type="text" name="Email" required placeholder="Email" />
 
         <input type="text" name="Website" placeholder="Website" />
 
-        <input type="button" name="next" class="next action-button" value="Next" />
+        <input type="button" name="next"  class="next action-button"  value="Next" />
+        
 
     </fieldset>
 
@@ -97,9 +98,19 @@
        <input type = "textarea" name ="Description" placeholder="Institution/ Organization"/>
        <input type ="text" name = "Project" placeholder="Certificate Name" />
        <input type = "textarea" name ="Description" placeholder="Institution/ Organization"/>
-       <input type="button" name="previous" class="previous action-button" value="Previous" />
+       
+       <div id='TextBoxesGroup'>
+	<div id="TextBoxDiv1">
+		<input type='textbox' id='textbox1' >
+	</div>
+</div>
+        <input type='button' value='Add certificates' id='addButton'>
+<input type='button' value='Remove certificates' id='removeButton'>
 
+        <input type="button" name="previous" class="previous action-button" value="Previous" />
         <input type="button" name="next" class="next action-button" value="Next" />
+        
+
 
 </fieldset>
 <fieldset>
@@ -109,9 +120,11 @@
        <input type ="text" name = "Project" placeholder="Data Science Skills" />
        <input type = "textarea" name ="Description" placeholder="Other Skills"/>
        
-       <input type="button" name="previous" class="previous action-button" value="Previous" />
+        
+        <input type="button" name="previous" class="previous action-button" value="Previous" />
 
         <input type="button" name="next" class="next action-button" value="Next" />
+        
 
 </fieldset>
 <fieldset>
@@ -128,15 +141,19 @@
        <input type="button" name="previous" class="previous action-button" value="Previous" />
 
         <input type="button" name="submit" class="submit action-button" value="Submit" />
-
+        
 
 </fieldset>
+
+
 </form>
 
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js'></script>
+<script src ='https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js'></script>
 
+<script src="lib/jquery.validate.js"></script>
 <script  src="index.js"></script>
 
 </body>
