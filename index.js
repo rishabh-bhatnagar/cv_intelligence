7e0879e8220399c8fd9704971fa3d96a414b6d6c
Sonia Singla <ssoniyaster@gmail.com>
2018-12-09 18:45:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,93 +1,219 @@
 $(document).ready(function(){
+   $('#clear-form').on('click', function()
+    { 
+        $('#msform').find('input:text, textarea').val('');
+   });
 var current_fs, next_fs, previous_fs; //fieldsets
+
 var left, opacity, scale; //fieldset properties which we will animate
+
 var animating; //flag to prevent quick multi-click glitches
-var isValid = false;	
+var isValid = false; 
 try{
-    $(".next").click(function(){
-      $("fieldset").children().not("h2, h3, button").each(function(){
-        if(!$(this).val()){
-        isValid = false;
-        alert("invalid");
+ var counter = 2;
+		
+    $("#addButton").click(function () {
+				
+	if(counter>10){
+            alert("Only 10 textboxes allow");
+            return false;
+	}   
+		
+	var newTextBoxDiv = $(document.createElement('div'))
+	     .attr("id", 'TextBoxDiv' + counter);
+                
+	newTextBoxDiv.after().html('<label>  </label>' +
+	      '<input type="text" name="textbox' + counter + 
+	      '" id="textbox' + counter + '" value="" >');
+            
+	newTextBoxDiv.appendTo("#TextBoxesGroup");
+
+				
+	counter++;
+     });
+
+     $("#removeButton").click(function () {
+	if(counter==1){
+          alert("No more textbox to remove");
+          return false;
+       }   
+        
+	counter--;
+			
+        $("#TextBoxDiv" + counter).remove();
+			
+     });
+   
+    
+ $(".next").click(function(){
+       $("fieldset").children().not("h2, h3, button").each(function(){
+
+          if(!$(this).val()){
+
+            isValid = false;
+
+            alert("invalid");
+
     }else{
+
         isValid = true;  
+
     } 
+
     })
         if(animating) return false;
+
         animating = true;
 
+
+
         current_fs = $(this).parent();
+
         next_fs = $(this).parent().next();
 
+
+
         //activate next step on progressbar using the index of next_fs
+
         $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 
+
+
         //show the next fieldset
+
         next_fs.show();
+
         //hide the current fieldset with style
+
         current_fs.animate({opacity: 0}, {
+
             step: function(now, mx) {
+
                 //as the opacity of current_fs reduces to 0 - stored in "now"
+
                 //1. scale current_fs down to 80%
+
                 scale = 1 - (1 - now) * 0.2;
+
                 //2. bring next_fs from the right(50%)
+
                 left = (now * 50)+"%";
+
                 //3. increase opacity of next_fs to 1 as it moves in
+
                 opacity = 1 - now;
+
                 current_fs.css({
+
                     'transform': 'scale('+scale+')',
+
                     'position': 'absolute'
+
                 });
+
                 next_fs.css({'left': left, 'opacity': opacity});
+
             },
+
             duration: 800,
+
             complete: function(){
+
                 current_fs.hide();
+
                 animating = false;
+
             },
+
             //this comes from the custom easing plugin
+
             easing: 'easeInOutBack'
+
         });
+
     });
+
     $(".previous").click(function(){
+
         if(animating) return false;
+
         animating = true;
 
+
+
         current_fs = $(this).parent();
+
         previous_fs = $(this).parent().prev();
 
+
+
         //de-activate current step on progressbar
+
         $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
 
+
+
         //show the previous fieldset
+
         previous_fs.show();
+
         //hide the current fieldset with style
+
         current_fs.animate({opacity: 0}, {
+
             step: function(now, mx) {
+
                 //as the opacity of current_fs reduces to 0 - stored in "now"
+
                 //1. scale previous_fs from 80% to 100%
+
                 scale = 0.8 + (1 - now) * 0.2;
+
                 //2. take current_fs to the right(50%) - from 0%
+
                 left = ((1-now) * 50)+"%";
+
                 //3. increase opacity of previous_fs to 1 as it moves in
+
                 opacity = 1 - now;
+
                 current_fs.css({'left': left});
+
                 previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+
             },
+
             duration: 800,
+
             complete: function(){
+
                 current_fs.hide();
+
                 animating = false;
+
             },
+
             //this comes from the custom easing plugin
+
             easing: 'easeInOutBack'
+
         });
+
     });
+
     $(".submit").click(function(){
+
         return false;
+
     });
+   
+    
 }
+
 catch (e) {
+
 	alert(e);
+
 }
+
 })
